c338c92eac92bdecf5a5a518fc974e388186179c
Bhaskar <bkotu@navaratan.com>
2014-05-27 12:11:35 +0530

    adding supporting api for json response testing

---
+++ features/step_definitions/api_steps.rb
@@ -0,0 +1,15 @@
+When(/^I request: \/stories\.json$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I get a (\d+) \(success\) status result$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the response header Content\-Type matches application\/json$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the response should match json:$/) do |string|
+  pending # express the regexp above with the code you wish you had
+end
